5104f273410688b8a999998ebac4792c65e3b763
jakobehmsen <jakobehmsen.github@gmail.com>
2018-01-16 20:26:07 +0100

    Finished #20.

--- src/Netbeans/Liveobjects/src/main/java/com/mycompany/liveobjects/Main.java
+++ src/Netbeans/Liveobjects/src/main/java/com/mycompany/liveobjects/Main.java
@@ -94,7 +94,7 @@ public class Main {
         
         frame.getContentPane().add(splitPane, BorderLayout.CENTER);
         
-        frame.setSize(400, 300);
+        frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
